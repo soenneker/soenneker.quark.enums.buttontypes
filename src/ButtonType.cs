fa71a4a03df9397ee17982bc9a0a3935589d37e0
Jake Soenneker <jake@soenneker.com>
2025-09-16 08:45:37 -0500

    Initial commit

---
+++ src/ButtonType.cs
@@ -0,0 +1,18 @@
+﻿using Intellenum;
+
+namespace Soenneker.Quark.Enums.ButtonTypes;
+
+/// <summary>
+/// An HTML CSS keyword set in .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public sealed partial class ButtonType
+{
+    public static readonly ButtonType Button = new("button");
+
+    public static readonly ButtonType Submit = new("submit");
+
+    public static readonly ButtonType Reset = new("reset");
+
+    public static readonly ButtonType Link = new("link");
+}
---
+++ test/Soenneker.Quark.Enums.ButtonTypes.Tests/ButtonTypeTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.ButtonTypes.Tests;
+
+public sealed class ButtonTypeTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
